a5ba7285a76a8cb8d9c4625ff1e8424ee20aba7f
Poj75014 <31952411+Poj75014@users.noreply.github.com>
2018-01-31 15:25:09 +0100

    modif boost et jout particle

---
+++ Assets/Scripts/Drivetrain/Boost.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace RVP
+{
+    public class Boost : MonoBehaviour {
+
+        private Rigidbody carRigidbody;
+        private VehicleParent vp;
+        public float boostForce = 50000;
+        public float downforceDuringBoost = 200;
+        public ParticleSystem[] boostParticles;
+
+        // Use this for initialization
+        void Start() {
+            vp = GetComponent<VehicleParent>();
+            carRigidbody = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            bool boosting = vp.boostButton;
+            if (boosting)
+            {
+                carRigidbody.AddForce(transform.forward * boostForce);
+                carRigidbody.AddForce(transform.up * -downforceDuringBoost);
+
+            }
+            //Play boost particles
+            if (boostParticles.Length > 0)
+            {
+                foreach (ParticleSystem curBoost in boostParticles)
+                {
+                    if (boosting && curBoost.isStopped)
+                    {
+                        curBoost.Play();
+                    }
+                    else if (!boosting && curBoost.isPlaying)
+                    {
+                        curBoost.Stop();
+                    }
+                }
+            }
+        }
+    }
+}
