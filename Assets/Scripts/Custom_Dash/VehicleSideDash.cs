4435046087a526a024bf068c037b8b5ce8a9e5ba
Poj75014 <31952411+Poj75014@users.noreply.github.com>
2018-01-31 11:29:37 +0100

    modif conduite

--- Assets/Scripts/Custom_Dash/VehicleSideDash.cs
+++ Assets/Scripts/Custom_Dash/VehicleSideDash.cs
@@ -25,9 +25,9 @@ public class VehicleSideDash : SideDash
 
     private void FixedUpdate ()
     {
-        if (Input.GetKeyDown(KeyCode.A) && base.available)
+        if (Input.GetKeyDown(KeyCode.Joystick1Button4) && base.available)
             this.Jump(Direction.left);
-        if (Input.GetKeyDown(KeyCode.E) && base.available)
+        if (Input.GetKeyDown(KeyCode.Joystick1Button5) && base.available)
             this.Jump(Direction.right);
     }
 
