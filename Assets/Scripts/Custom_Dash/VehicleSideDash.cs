a5e1c9c830147d36337d537768b4d75703892d93
Gaoura <gaoura@outlook.com>
2018-01-31 11:23:09 +0100

    VehicleSideDash : la vitesse varie maintenant selon une AnimationCurve

--- Assets/Scripts/Custom_Dash/VehicleSideDash.cs
+++ Assets/Scripts/Custom_Dash/VehicleSideDash.cs
@@ -16,6 +16,8 @@ public class VehicleSideDash : MonoBehaviour
 
 
     private Rigidbody vehicleRigidbody;
+    [SerializeField]
+    private AnimationCurve speedVariation = new AnimationCurve(new Keyframe(0f, 0.1f), new Keyframe(0.1f, 1f), new Keyframe(0.9f, 1f), new Keyframe(1f, 0f));
 
 
     private void OnValidate()
@@ -32,27 +34,41 @@ public class VehicleSideDash : MonoBehaviour
     private void FixedUpdate ()
     {
         if (Input.GetKeyDown(KeyCode.A) && this.available)
-            this.Jump(Direction.left);
+            this.Dash(Direction.left);
         if (Input.GetKeyDown(KeyCode.E) && this.available)
-            this.Jump(Direction.right);
+            this.Dash(Direction.right);
     }
 
-    private void Jump(Direction direction)
+    private void Dash(Direction direction)
+    {
+        //Vector2 angle = MathsTools.DegreesToVector2(this.dashAngleInDegrees);
+        Vector3 originalSpeed = this.vehicleRigidbody.velocity;
+        //this.vehicleRigidbody.useGravity = false;
+        //this.vehicleRigidbody.velocity = transform.TransformVector(new Vector3(angle.x * (int)direction, -angle.y) * this.speed)
+        //                                + originalSpeed;
+        StartCoroutine(VarySpeedDuringDash(direction, originalSpeed));
+        StartCoroutine(AutoStop(direction, originalSpeed));
+        StartCoroutine(Cooldown());
+    }
+
+    private IEnumerator VarySpeedDuringDash(Direction direction, Vector3 originalSpeed)
     {
         Vector2 angle = MathsTools.DegreesToVector2(this.dashAngleInDegrees);
-        Vector3 oldSpeed = this.vehicleRigidbody.velocity;
-        this.vehicleRigidbody.velocity = transform.TransformVector(new Vector3(angle.x * (int)direction, -angle.y) * this.speed)
-                                        + oldSpeed;
-        this.vehicleRigidbody.useGravity = false;
-        StartCoroutine(AutoStop(direction, oldSpeed));
-        StartCoroutine(Cooldown());
+        float step = this.duration / 100f;
+        for (float counter = 0f; counter < this.duration; counter += step)
+        {
+            float speedAccordingToCurve = this.speed * this.speedVariation.Evaluate(counter);
+            this.vehicleRigidbody.velocity = transform.TransformVector(new Vector3(angle.x * (int)direction, -angle.y) * speedAccordingToCurve)
+                                            + originalSpeed;
+            yield return new WaitForSeconds(step);
+        }
     }
 
     private IEnumerator AutoStop(Direction direction, Vector3 oldSpeed)
     {
         yield return new WaitForSeconds(this.duration);
         this.vehicleRigidbody.velocity = oldSpeed;
-        this.vehicleRigidbody.useGravity = true;
+        //this.vehicleRigidbody.useGravity = true;
     }
 
     private IEnumerator Cooldown()
