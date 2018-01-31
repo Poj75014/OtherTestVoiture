21a8f425ad3645b7dc4f016a83cc23e15c4d00c4
Poj75014 <31952411+Poj75014@users.noreply.github.com>
2018-02-01 00:01:24 +0100

    jump joystick

--- Assets/Scripts/Flo/Jump.cs
+++ Assets/Scripts/Flo/Jump.cs
@@ -29,7 +29,7 @@ public class Jump : MonoBehaviour
 	
 	void FixedUpdate ()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && this.available)
+        if (Input.GetKeyDown(KeyCode.Joystick1Button3) && this.available)
             this.Execute();
     }
     
