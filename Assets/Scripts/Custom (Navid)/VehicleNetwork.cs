4a03a270cf6b2f4934fc3951d1a59370cf99fae3
Navid <navid.soroosh.markazi@gmail.com>
2018-02-01 01:40:24 +0100

    Network

---
+++ Assets/Scripts/Custom (Navid)/VehicleNetwork.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace RVP
+{
+    public class VehicleNetwork : NetworkBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            if (!isLocalPlayer)
+            {
+                this.GetComponent<BasicInput>().enabled = false;
+            }
+            else
+            {
+                Camera.main.GetComponent<CameraControl>().target = this.transform;
+                Camera.main.GetComponent<CameraControl>().Initialize();
+            }
+        }
+    }
+}
